32ac7b173ce20c5e1637130b0a0465ad3f0eb21d
nasubi-dev <r916nis@gmail.com>
2026-01-08 15:05:24 +0900

    調整

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,4 @@
-from models.db import db
-from models.user import User
-from routes.game import game_bp
-blueprints = [
-    game_bp
-]
+from .db import db
+from .user import User
 
-__all__ = ["db", "User", "blueprints"]
+__all__ = ["db", "User"]
